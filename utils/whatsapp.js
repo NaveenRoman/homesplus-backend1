611e0f50df6146dce3f6cf8d35e91db3c4362846
NaveenRoman <naveenrom232@gmail.com>
2026-02-13 13:48:03 +0530

    Final whatsapp message  upload working

--- utils/whatsapp.js
+++ utils/whatsapp.js
@@ -1,16 +1,10 @@
-const twilio = require("twilio");
-
-const client = twilio(
-  process.env.TWILIO_SID,
-  process.env.TWILIO_AUTH
-);
-
 async function sendWhatsApp(message) {
+  console.log("FROM:", process.env.TWILIO_WHATSAPP);
+  console.log("TO:", process.env.ADMIN_WHATSAPP);
+
   return client.messages.create({
     from: process.env.TWILIO_WHATSAPP,
     to: process.env.ADMIN_WHATSAPP,
     body: message,
   });
 }
-
-module.exports = sendWhatsApp;
